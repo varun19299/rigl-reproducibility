9aa37837b958023d88de9ce50ba583315f7814c7
Varun Sundar <vsundar4@wisc.edu>
2020-10-29 16:21:48 +0530

    Added setup file for pip install (editable local)

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sparselearning", # Replace with your own username
+    version="0.1",
+    author="Varun Sundar, Rajat Vadiraj",
+    author_email="vsundar4@wisc.edu, rajatvd@gmail.com",
+    description="Rethinking Dynamic Sparse learning techniques.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/varun19299/rethinking-sparse-learning",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
